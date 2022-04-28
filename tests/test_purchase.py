2ca20f615a72aac13ac5e0de5e2ba0567f7125fe
Karim-Dorado <karimdorado@gmail.com>
2022-04-28 09:09:26 +0200

    add test for more than 12 places

--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -11,7 +11,7 @@ def test_success_booking_places(client):
         data={
             'club':club,
             'competition':competition,
-            'places':13
+            'places':10
             }
     )
     assert response.status_code == 200
@@ -19,9 +19,9 @@ def test_success_booking_places(client):
     assert data.find("Great-booking complete!")
 
 
-def test_fail_booking_places(client):
-    club = "Simply Lift"
-    email = "john@simplylift.co"
+def test_to_much_booking_places(client):
+    club = "Iron Temple"
+    email = "admin@irontemple.com"
     competition = "Fall Classic"
     login = client.post('/showSummary',  data={"email": email, "name": club}, follow_redirects=True)
     assert login.status_code == 200
@@ -30,7 +30,7 @@ def test_fail_booking_places(client):
         data={
             'club':club,
             'competition':competition,
-            'places':14
+            'places':6
             }
     )
     assert response.status_code == 200
@@ -55,3 +55,21 @@ def test_fail_negative_booking_places(client):
     assert response.status_code == 200
     data = response.data.decode()
     assert data.find("Please, enter a positive number!")
+
+def test_purchase_more_than_12_places(client):
+    club = "Simply Lift"
+    email = "john@simplylift.co"
+    competition = "Fall Classic"
+    login = client.post('/showSummary',  data={"email": email, "name": club}, follow_redirects=True)
+    assert login.status_code == 200
+    response = client.post(
+        '/purchasePlaces',
+        data={
+            'club':club,
+            'competition':competition,
+            'places':13
+            }
+    )
+    assert response.status_code == 200
+    data = response.data.decode()
+    assert data.find("You can't require more than 12 places")
